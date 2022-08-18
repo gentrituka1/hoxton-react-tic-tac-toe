cc6a6a5ed96079586473dd0d17395ced50d20692
Gentrit Uka <gentrit-uka@live.com>
2022-08-18 20:09:39 +0200

    updated

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,9 +4,10 @@ import { Square } from "./Square";
 export function Board (){
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isX, setIsX] = useState(true);
-    const [status, setStatus] = useState(null);
 
     function handleClick (position: number) {
+        if(calculateWinner(squares) || squares[position]) return
+
         const squaresCopy = [...squares];
         squaresCopy[position] = isX ? 'X' : 'O';
         setSquares(squaresCopy);
@@ -16,11 +17,39 @@ export function Board (){
     function restartGame(){
         setSquares(Array(9).fill(null));
         setIsX(true);
-        setStatus(null);
     }
 
+    function calculateWinner(squares: string[]) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let element of lines){
+            if (squares[element[0]] && squares[element[0]] === squares[element[1]] && squares[element[1]] === squares[element[2]]){
+                return squares[element[0]];
+            }
+        }
+        return null;
+    }
+
+    
+        const winner = calculateWinner(squares);
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        }
+        else{
+            status = `Now Playing: ${isX ? 'X' : 'O'}`;
+        }
 
     return (
+        <>
         <div className="board">
             <div className="board-row">
                 <Square value={squares[0]} onClick={() => handleClick(0)}/>
@@ -37,8 +66,9 @@ export function Board (){
                 <Square value={squares[7]} onClick={() => handleClick(7)}/>
                 <Square value={squares[8]} onClick={() => handleClick(8)}/>
             </div>
-            <div className="status">{status}</div>
-            <button className="restart" onClick={restartGame}>Restart</button>
         </div>
+        <div className="status">{status}</div>
+        <button className="restart" onClick={restartGame}>Restart</button>
+        </>
     )
 }
